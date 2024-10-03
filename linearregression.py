cf55458406a0de45ca37db3197f0348a84d94350
YoavMor2004 <yoavdor9@gmail.com>
2024-10-03 17:02:38 +0300

    Renamed linearregression.model.features to labels for better consistency

--- linearregression.py
+++ linearregression.py
@@ -7,13 +7,13 @@ N = TypeVar('N', bound=int)
 P = TypeVar('P', bound=int)
 
 
-def model(features: ndarray[tuple[N], dtype[uint8]], traces: ndarray[tuple[N, P], dtype[float64]])\
+def model(labels: ndarray[tuple[N], dtype[uint8]], traces: ndarray[tuple[N, P], dtype[float64]])\
         -> ndarray[tuple[Literal[9], P], dtype[float64]]:
 
     x: ndarray[tuple[Literal[9], N], dtype[uint8]]
     x = np.concatenate((
-        np.unpackbits(features[np.newaxis, :], axis=0, bitorder='little'),
-        np.ones((1, features.size))
+        np.unpackbits(labels[np.newaxis, :], axis=0, bitorder='little'),
+        np.ones((1, labels.size))
     ), axis=0, dtype=uint8)
 
     return np.linalg.inv(x @ x.T) @ x @ traces
