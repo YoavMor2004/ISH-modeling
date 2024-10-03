7f3540996a0812affef8f3854c446a180b879d77
YoavMor2004 <yoavdor9@gmail.com>
2024-10-03 17:05:55 +0300

    In template.model: Added support for calculating coefficients vector for multiple POI's

--- template.py
+++ template.py
@@ -6,23 +6,24 @@ import numpy as np
 from numpy import ndarray, dtype, uint8, float64
 
 N = TypeVar('N', bound=int)
+P = TypeVar('P', bound=int)
 
 
 @dataclass
-class TemplateModel:
-    mean_of_classes: ndarray[tuple[Literal[256]], dtype[float64]]
-    std_of_classes: ndarray[tuple[Literal[256]], dtype[float64]]
+class TemplateModel(Generic[P]):
+    mean_of_classes: ndarray[tuple[Literal[256], P], dtype[float64]]
+    std_of_classes: ndarray[tuple[Literal[256], P], dtype[float64]]
 
 
-def model(labels: ndarray[tuple[N], dtype[uint8]], traces: ndarray[tuple[N], dtype[float64]]) -> TemplateModel:
-    mean_of_classes: ndarray[tuple[Literal[256]], dtype[float64]]
-    std_of_classes: ndarray[tuple[Literal[256]], dtype[float64]]
+def model(labels: ndarray[tuple[N], dtype[uint8]], traces: ndarray[tuple[N, P], dtype[float64]]) -> TemplateModel[P]:
+    mean_of_classes: ndarray[tuple[Literal[256], P], dtype[float64]]
+    std_of_classes: ndarray[tuple[Literal[256], P], dtype[float64]]
 
-    mean_of_classes = np.empty(256, dtype=float64)
-    std_of_classes = np.empty(256, dtype=float64)
+    mean_of_classes = np.empty((256, traces.shape[1]), dtype=float64)
+    std_of_classes = np.empty((256, traces.shape[1]), dtype=float64)
     for x in range(256):
-        c: ndarray[tuple[int], dtype[float64]]
+        c: ndarray[tuple[int, P], dtype[float64]]
         c = traces[labels == x]
-        mean_of_classes[x] = c.mean()
-        std_of_classes[x] = c.std()
+        mean_of_classes[x] = c.mean(axis=0)
+        std_of_classes[x] = c.std(axis=0)
     return TemplateModel(mean_of_classes, std_of_classes)
