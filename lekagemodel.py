4d7e8e6ab19eb7599f563cb982182b3ed9604b07
YoavMor2004 <yoavdor9@gmail.com>
2024-10-07 18:33:29 +0300

    Updated LeakageModel

--- lekagemodel.py
+++ lekagemodel.py
@@ -10,13 +10,13 @@ P = TypeVar('P', bound=int)
 K = TypeVar('K', bound=int)
 
 
-class LeakageModel(ABC, Generic[P]):
+class LeakageModel(ABC, Generic[B, P]):
     @abstractmethod
     def keys_probability(
             self,
             plaintexts: ndarray[tuple[B, N],    dtype[uint8]],
             traces:     ndarray[tuple[B, N, P], dtype[float64]],
-            keys:       ndarray[tuple[B, K],       dtype[uint8]]
+            keys:       ndarray[tuple[B, K],    dtype[uint8]]
     ) -> ndarray[tuple[B, K, P], dtype[float64]]:
         pass
 
@@ -31,6 +31,6 @@ class LeakageModel(ABC, Generic[P]):
             traces,
             cast(
                 ndarray[tuple[B, Literal[256]], dtype[uint8]],
-                np.broadcast_to(np.arange(256, dtype=uint8), (plaintexts.shape[0], 256))
+                np.broadcast_to(np.arange(256, dtype=uint8), (traces.shape[0], 256))
             )
         ).argmax(axis=1)
