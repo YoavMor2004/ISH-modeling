eefbff2c1e1e592b6b8ff528839667a282aea573
YoavMor2004 <yoavdor9@gmail.com>
2024-10-08 02:38:39 +0300

    Refactored code massively (it actually makes sense now)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from typing import Optional, cast, Literal, TypeVar
+from typing import Optional, Literal, TypeVar, Iterator, NamedTuple, Self, Generic, cast, Iterable
 
 import numpy as np
 from numpy import ndarray, dtype, int64, uint8, float64
 
 import linearregression as lr
 import template
-from resourceloader import Resources, Attack
+from lekagemodel import LeakageModel
+from resourceloader import Resources
 
 B = TypeVar('B', bound=int)
 N = TypeVar('N', bound=int)
@@ -22,68 +23,106 @@ def ndarray_repr(x: ndarray[tuple, dtype], /, name: Optional[str] = None) -> str
     return f'{x.dtype} {name}{tuple_str(x.shape)}'
 
 
-def is_ndarray(x: ndarray[tuple, dtype], shape: tuple[Optional[int], ...], dt: type, /) -> bool:
-    return (isinstance(x, ndarray) and
-            x.ndim == len(shape)
-            and all(shape[d] is None or x.shape[d] == shape[d] for d in range(x.ndim))
-            and x.dtype == dt)
+def is_compatible_shape(x: ndarray[tuple, dtype], shape: tuple[Optional[int], ...], /) -> bool:
+    # _TODO: Double pointer non-deterministic algorithm for weeding out 1-dimensions
+    return all((
+        len(shape) <= x.ndim,
+        all(d == 1 for d in x.shape[len(shape):]),
+        all(shape[d] in {x.shape[d], None} for d in range(len(shape)))
+    ))
 
 
-def profile(res: Resources, pois: ndarray[tuple[B], dtype[int64]])\
-        -> Optional[tuple[template.Model[B], lr.Model[B]]]:
+def is_valid_ndarray(x: ndarray[tuple, dtype], shape: tuple[Optional[int], ...], dt: type, /) -> bool:
+    return is_compatible_shape(x, shape) and x.dtype == dt
 
-    profile_data = res.load_profile('profile')
-    if profile_data is None:
-        return print('no profile file found')
-    if profile_data['labels'].shape[0] != pois.size:
-        return print('profile and pois have different block counts')
-    labels = cast(ndarray[tuple[B, int], dtype[uint8]], profile_data['labels'])
-    traces = cast(ndarray[tuple[B, int], dtype[float64]], profile_data['traces'][np.arange(pois.size), :, pois])
-    del profile_data
+
+class Data(NamedTuple, Generic[B, N]):
+    profiling_labels:     ndarray[tuple[B, N], dtype[uint8]]
+    profiling_traces:     ndarray[tuple[B, N], dtype[float64]]
+    attacking_plaintexts: ndarray[tuple[B, N], dtype[uint8]]
+    attacking_traces:     ndarray[tuple[B, N], dtype[float64]]
+
+    @classmethod
+    def load(cls, resources_filepath, block_count: B, trace_count: N) -> Self | str:
+        profiling_labels:     ndarray[tuple[B, N], dtype[uint8]]
+        profiling_traces:     ndarray[tuple[B, N], dtype[float64]]
+        attacking_plaintexts: ndarray[tuple[B, N], dtype[uint8]]
+        attacking_traces:     ndarray[tuple[B, N], dtype[float64]]
+
+        res = Resources.new(resources_filepath)
+        if res is None:
+            return 'resources file not found'
+
+        poi_file = res.load('poi_file')
+        if poi_file is None:
+            return 'poi file not found'
+        if res['poi'] not in poi_file:
+            return 'poi not found'
+        if not is_valid_ndarray(poi_file[res['poi']].squeeze(), (block_count,), int64):
+            return 'poi ndarray is not properly formed'
+        poi: ndarray[tuple[B], dtype[int64]] = poi_file[res['poi']].squeeze()
+        del poi_file
+
+        profile_file = res.load('profile')
+        if profile_file is None:
+            return 'profile file not found'
+        if 'labels' not in profile_file:
+            return 'profiling labels not found'
+        if 'traces' not in profile_file:
+            return 'profiling traces not found'
+        if not is_valid_ndarray(profile_file['labels'], (block_count, trace_count), uint8):
+            return 'profiling labels ndarray is not properly formed'
+        if not is_valid_ndarray(profile_file['traces'], (block_count, trace_count, None), float64):
+            return 'profiling traces ndarray is not properly formed'
+        profiling_labels = profile_file['labels'].reshape(block_count, trace_count)
+        profiling_traces = profile_file['traces'].reshape(block_count, trace_count, -1)[np.arange(block_count), :, poi]
+        del profile_file
+
+        attacking_file = res.load('attack')
+        if attacking_file is None:
+            return 'attacking file not found'
+        if 'labels' not in attacking_file:
+            return 'attacking plaintexts not found'
+        if 'traces' not in attacking_file:
+            return 'attacking traces not found'
+        if not is_valid_ndarray(attacking_file['labels'], (block_count, trace_count), uint8):
+            return 'attacking labels ndarray is not properly formed'
+        if not is_valid_ndarray(attacking_file['traces'], (block_count, trace_count, None), float64):
+            return 'attacking traces ndarray is not properly formed'
+        attack_plaintexts = attacking_file['labels'].reshape(block_count, trace_count)
+        attack_traces = attacking_file['traces'].reshape(block_count, trace_count, -1)[np.arange(block_count), :, poi]
+        del attacking_file
+
+        return cls(profiling_labels, profiling_traces, attack_plaintexts, attack_traces)
+
+
+def profile(
+        labels: ndarray[tuple[B, N], dtype[uint8]],
+        traces: ndarray[tuple[B, N], dtype[float64]]
+) -> tuple[template.Model[B], lr.Model[B]]:
 
     return template.Model[B](labels, traces), lr.Model[B](labels, traces)
 
 
-def attack(res: Resources, pois: ndarray[tuple[B], dtype[int64]]) -> None:
-    temp = profile(res, pois)
-    if temp is None:
-        return
-    template_model, lr_model = temp
-    del temp
-
-    plaintexts: ndarray[tuple[B, int], dtype[uint8]]
-    traces: ndarray[tuple[B, int], dtype[float64]]
-
-    attack_data = res.load_profile('attack')
-    if attack_data is None:
-        return print('no attack file found')
-    if attack_data['traces'].shape[0] != pois.size:
-        return print('attack and pois have different block counts')
-    plaintexts = cast(ndarray[tuple[B, int], dtype[uint8]], attack_data['labels'])
-    traces = cast(ndarray[tuple[B, int], dtype[float64]], attack_data['traces'][np.arange(pois.size), :, pois])
-    del attack_data
+def attack(
+        plaintexts: ndarray[tuple[B, N], dtype[uint8]],
+        traces:     ndarray[tuple[B, N], dtype[float64]],
+        models: Iterable[tuple[str, LeakageModel[B]]]
+) -> None:
 
     np.set_printoptions(formatter={'int': hex})
-    print('template keys:')
-    print(template_model.get_key(plaintexts, traces), '\n')
-    print('lr keys:')
-    print(lr_model.get_key(plaintexts, traces), '\n')
+    for name, model in models:
+        print(f'{name}:')
+        print(model.get_key(plaintexts, traces), '\n')
 
 
 def main() -> None:
-    res = Resources.new('resources.json')
-    if res is None:
-        return print('no resources.json file found')
-
-    snr_pois: ndarray[tuple[Literal[16]], dtype[int64]]
-    poi = res.load('poi')
-    if poi is None:
-        return print('no poi file found')
-    if not is_ndarray(poi['snr_pois'].squeeze(), (16, ), int64):
-        return print('snr_pois is ill-formed')
-    snr_pois = poi['snr_pois'].squeeze()
-    del poi
-    attack(res, snr_pois)
+    data = Data[Literal[16], Literal[4096]].load('resources.json', 16, 4096)
+    if isinstance(data, str):
+        return print(data)
+    template_model = template.Model[Literal[16]](data.profiling_labels, data.profiling_traces)
+    lr_model = lr.Model[Literal[16]](data.profiling_labels, data.profiling_traces)
+    attack(data.attacking_plaintexts, data.attacking_traces, (('template', template_model), ('lr', lr_model)))
 
 
 if __name__ == '__main__':
--- resourceloader.py
+++ resourceloader.py
@@ -5,56 +5,35 @@ from numpy import ndarray, dtype, uint8, float64
 from scipy.io import loadmat  # type: ignore
 
 
-class Profile(TypedDict):
-    labels: ndarray[tuple[int, int], dtype[uint8]]
-    traces: ndarray[tuple[int, int, int], dtype[float64]]
-
-
-class Attack(TypedDict):
-    plaintexts: ndarray[tuple[int, int], dtype[uint8]]
-    traces: ndarray[tuple[int, int, int], dtype[float64]]
-
-
-T = TypeVar('T', Profile, Attack)
-
-
-def profile_new(d: dict[str, ndarray[tuple, dtype[Any]]], /) -> Optional[Profile]:
-    return verify(d, Profile, 'labels', 'traces')
-
-
-def attack_new(d: dict[str, ndarray[tuple, dtype[Any]]], /) -> Optional[Attack]:
-    return verify(d, Attack, 'plaintexts', 'traces')
-
-
-def verify(
-        d: dict[str, ndarray[tuple, dtype[Any]]],
-        /,
-        profile_type: Type[T],
-        labels_name: str,
-        traces_name: str
-) -> Optional[T]:
-    if labels_name not in d:
-        return None
-    if traces_name not in d:
-        return None
-    labels = d[labels_name]
-    traces = d[traces_name]
-    del d
-    if labels.ndim < 2:
-        return None
-    if traces.ndim < 3:
-        return None
-    labels = labels.reshape(labels.shape[:2])
-    traces = traces.reshape(traces.shape[:3])
-    if labels.shape != traces.shape[0:2]:
-        return None
-    if labels.dtype != uint8:
-        return None
-    if traces.dtype != float64:
-        return None
-    labels = cast(ndarray[tuple[int, int],      dtype[uint8]],   labels)
-    traces = cast(ndarray[tuple[int, int, int], dtype[float64]], traces)
-    return profile_type({labels_name: labels, traces_name: traces})  # type: ignore
+# def verify(
+#         d: dict[str, ndarray[tuple, dtype[Any]]],
+#         /,
+#         profile_type: Type[T],
+#         labels_name: str,
+#         traces_name: str
+# ) -> Optional[T]:
+#     if labels_name not in d:
+#         return None
+#     if traces_name not in d:
+#         return None
+#     labels = d[labels_name]
+#     traces = d[traces_name]
+#     del d
+#     if labels.ndim < 2:
+#         return None
+#     if traces.ndim < 3:
+#         return None
+#     labels = labels.reshape(labels.shape[:2])
+#     traces = traces.reshape(traces.shape[:3])
+#     if labels.shape != traces.shape[0:2]:
+#         return None
+#     if labels.dtype != uint8:
+#         return None
+#     if traces.dtype != float64:
+#         return None
+#     labels = cast(ndarray[tuple[int, int],      dtype[uint8]],   labels)
+#     traces = cast(ndarray[tuple[int, int, int], dtype[float64]], traces)
+#     return profile_type({labels_name: labels, traces_name: traces})  # type: ignore
 
 
 class Resources(dict[str, str]):
@@ -76,16 +55,6 @@ class Resources(dict[str, str]):
             return None
 
         return cls(resources)
-
-    def load_profile(self, file_name: str) -> Optional[Profile]:
-        if file_name not in self:
-            return None
-        return profile_new(loadmat(self[file_name]))
-
-    def load_attack(self, file_name: str) -> Optional[Attack]:
-        if file_name not in self:
-            return None
-        return attack_new(loadmat(self[file_name]))
 
     def load(self, file_name: str) -> Optional[dict[str, ndarray[tuple, dtype]]]:
         if file_name not in self:
